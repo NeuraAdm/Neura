3f384ca38778b788acacc24649c2f63614498cb2
Neura <neura.admt@gmail.com>
2025-03-25 12:44:03 -0500

    update archives

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/Neura/',
+  build: {
+    outDir: 'docs'  // Cambia de 'dist' a 'docs'
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
